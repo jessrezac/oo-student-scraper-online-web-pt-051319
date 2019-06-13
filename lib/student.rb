cbb3430f08965e8f541d2d8d86f26f787d3b15ad
Jess Rezac <jess.rezac@gmail.com>
2019-06-13 14:20:24 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,10 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    student_hash.each do |k, v|
+      self.send("#{k}=", v)
+    end
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
@@ -17,7 +20,7 @@ class Student
   end
 
   def self.all
-    
+    @@all
   end
 end
 
